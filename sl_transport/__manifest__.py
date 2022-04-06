267ce4ce13e10aa2058e81b1bd55c0878d75a156
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2022-04-06 08:19:57 +0200

    Initial repo setup + addon skeleton

---
+++ sl_transport/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Stein & Gabelgaard ApS
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sl Transport',
+    'description': """
+        SL2022 Transport interface""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Jesper Darum,Stein & Gabelgaard ApS',
+    'website': 'www.steingabelgaard.dk',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
